be47abdfdf61435fb4e35a6a6efed40282410994
arrancy <ruturajchondekar@gmail.com>
2024-02-01 18:24:34 +0530

    changed file in db folder of 03-mongo

--- assignments-master/week-3/03-mongo/db/index.js
+++ assignments-master/week-3/03-mongo/db/index.js
@@ -1,27 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("mongoose-type-url");
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(
+  "mongodb+srv://ruturajchondekar:GlLErtjinPtOIZug@cluster0.qxfejac.mongodb.net/courseSellingApp"
+);
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  username: String,
+  password: String,
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  username: String,
+  password: String,
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  title: String,
+  description: String,
+  price: Number,
+  imagelink: mongoose.SchemaTypes.Url,
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+};
--- assignments-master/week-3/03-mongo/routes/admin.js
+++ assignments-master/week-3/03-mongo/routes/admin.js
@@ -1,18 +1,22 @@
+const mongoose = require("mongoose");
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db/index");
+const { User } = require("../db/index");
+const { Course } = require("../db/index");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, (req, res) => {
+  // Implement course creation logic
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, (req, res) => {
+  // Implement fetching all courses logic
 });
 
 module.exports = router;
