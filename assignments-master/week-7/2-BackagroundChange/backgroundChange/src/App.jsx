a5aec40e8d24715a280932a37ddb5cb99c21118a
arrancy <ruturajchondekar@gmail.com>
2024-03-12 21:28:08 +0530

    implemented the background change assignment without styling

---
+++ assignments-master/week-7/2-BackagroundChange/backgroundChange/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState, useRef } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [color, setColor] = useState("white");
+
+  const buttonRefs = new Array(7).fill(null).map(() => useRef());
+  let colorsArray = [
+    "white",
+    "black",
+    "red",
+    "blue",
+    "aqua",
+    "brown",
+    "purple",
+  ];
+
+  function handleClick(index) {
+    let currentRef = buttonRefs[index];
+    let currentColor = currentRef.current.innerHTML;
+    setColor(currentColor);
+  }
+
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: color,
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        {colorsArray.map((colour, index) => {
+          return (
+            <button
+              key={index}
+              ref={buttonRefs[index]}
+              onClick={() => {
+                handleClick(index);
+              }}
+            >
+              {colour}
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
